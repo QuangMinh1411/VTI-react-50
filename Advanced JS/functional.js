67db269d2ae81c77788e77f8ff8cfc1a1faae7b9
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2023-04-07 19:09:06 +0700

    JS

---
+++ Advanced JS/functional.js
@@ -0,0 +1,165 @@
+// const messages = [
+//   "They can be inserted into arrays",
+//   (message) => console.log(message),
+//   "like variables",
+//   (message) => console.log(message),
+// ];
+
+// messages[1](messages[0]); // They can be inserted into arrays
+// messages[3](messages[2]); // like variables
+
+// const insideFn = (logger) => {
+//   logger("They can be sent to other functions as arguments");
+// };
+// //Trien khai ham logger
+// insideFn((mes) => console.log(mes + " Hi"));
+
+const createScream = function (logger) {
+  return function (message) {
+    logger(message.toUpperCase() + "!!!");
+  };
+};
+// const createScream = (logger)=>{
+//   return (message)=>logger(message.toUpperCase() + "!!!");
+// }
+// const createScream = logger=>message=>logger(message.toUpperCase()+'!!!');
+
+const scream = createScream((message) => console.log(message));
+
+scream("functions can be returned from other functions");
+scream("createScream returns a function");
+scream("scream invokes that returned function");
+
+const rateColor = (color, rating) => ({
+  ...color,
+  rating,
+});
+
+const frederick = {
+  name: "Frederick Douglass",
+  canRead: false,
+  canWrite: false,
+};
+
+const selfEducate = (person) => ({
+  ...person,
+  canRead: true,
+  canWrite: true,
+  canRide: true,
+});
+
+console.log(selfEducate(frederick));
+console.log(frederick);
+
+const schools = ["Yorktown", "Washington & Liberty", "Wakefield"];
+const wSchools = schools.filter((school) => school[0] === "W");
+
+const cutSchool = (cut, list) => list.filter((school) => school !== cut);
+
+console.log(cutSchool("Washington & Liberty", schools).join(", "));
+
+const highSchools = schools.map((school) => `${school} High School`);
+
+console.log(highSchools.join("\n"));
+
+// let schools = [
+//     { name: "Yorktown" },
+//     { name: "Stratford" },
+//     { name: "Washington & Liberty" },
+//     { name: "Wakefield" }
+//   ];
+
+//   let updatedSchools = editName("Stratford", "HB Woodlawn", schools);
+
+//   console.log(updatedSchools[1]); // { name: "HB Woodlawn" }
+//   console.log(schools[1]); // { name: "Stratford" }
+
+const editName = (oldName, name, arr) =>
+  arr.map((item) => {
+    if (item.name === oldName) {
+      return {
+        ...item,
+        name,
+      };
+    } else {
+      return item;
+    }
+  });
+
+const ages = [21, 18, 42, 40, 64, 63, 34];
+
+const maxAge = ages.reduce((max, age) => {
+  console.log(`${age} > ${max} = ${age > max}`);
+  if (age > max) {
+    return age;
+  } else {
+    return max;
+  }
+}, 0);
+
+const colors = [
+  {
+    id: "xekare",
+    title: "rad red",
+    rating: 3,
+  },
+  {
+    id: "jbwsof",
+    title: "big blue",
+    rating: 2,
+  },
+  {
+    id: "prigbj",
+    title: "grizzly grey",
+    rating: 5,
+  },
+  {
+    id: "ryhbhsl",
+    title: "banana",
+    rating: 1,
+  },
+];
+
+const hashColors = colors.reduce((hash, { id, title, rating }) => {
+  hash[id] = { title, rating };
+  return hash;
+}, {});
+
+console.log(hashColors);
+
+// const colors = ["red", "red", "green", "blue", "green"];
+
+// const uniqueColors = colors.reduce(
+//   (unique, color) =>
+//     unique.indexOf(color) !== -1 ? unique : [...unique, color],
+//   []
+// );
+
+// console.log(uniqueColors);
+
+const invokeIf = (condition, fnTrue, fnFalse) =>
+  condition ? fnTrue() : fnFalse();
+
+const showWelcome = () => console.log("Welcome!!!");
+
+const showUnauthorized = () => console.log("Unauthorized!!!");
+
+invokeIf(true, showWelcome, showUnauthorized); // "Welcome!!!"
+invokeIf(false, showWelcome, showUnauthorized); // "Unauthorized!!!"
+
+const countdown = (value, fn) => {
+  fn(value);
+  return value > 0 ? countdown(value - 1, fn) : value;
+};
+
+countdown(10, (value) => console.log(value));
+
+// const countdown = (value, fn, delay = 1000) => {
+//     fn(value);
+//     return value > 0
+//       ? setTimeout(() => countdown(value - 1, fn, delay), delay)
+//       : value;
+//   };
+
+//   const log = value => console.log(value);
+//   countdown(10, log);
---
+++ Advanced JS/scripts.js
@@ -0,0 +1,83 @@
+const sandwich = {
+  bread: "dutch crunch",
+  meat: "tuna",
+  cheese: "swiss",
+  toppings: ["lettuce", "tomato", "mustard"],
+};
+
+const { bread, meat } = sandwich;
+
+const regularPerson = {
+  firstName: "Howard",
+  lastName: "John",
+  spouse: {
+    firstName: "Mary",
+    lastName: "Select",
+    pets: {
+      name: "Miu",
+    },
+  },
+};
+// const lordify = ({
+//   spouse: {
+//     pets: { name },
+//   },
+// }) => {
+//   console.log(`${name} of person spouse pet`);
+// };
+// lordify(regularPerson);
+
+// const [, , thirdAnimal] = ["Horse", "Mouse", "Cat"];
+
+// const name = "Taillac";
+// const elevation = 9876;
+// const funHike = { name, elevation };
+// console.log(funHike);
+// const sound = "Bee";
+// const skier = {
+//   name,
+//   sound,
+//   powderYell() {
+//     let yell = this.sound.toUpperCase();
+//     console.log(`${yell} ${yell} ${yell}!!!`);
+//   },
+//   speed(mph) {
+//     this.speed = mph;
+//     console.log("speed:", mph);
+//   },
+// };
+// skier.speed(200);
+
+// //Spread operator
+// const peaks = ["Tallac", "Ralston", "Rose"];
+// const canyons = ["Ward", "Blackwood"];
+// const arr = [peaks[0], ...canyons];
+// const tahoe = [...peaks, ...canyons];
+
+// //Async
+// fetch("https://api.randomuser.me/?nat=US&results=1")
+//   .then((res) => res.json())
+//   .then((json) => json.results)
+//   .then(console.log)
+//   .catch(console.error);
+
+const getFakePerson = async () => {
+  let res = await fetch("https://api.randomuser.me/?nat=US&results=1");
+  let { results } = res.json();
+  console.log(results);
+};
+
+getFakePerson();
+
+// const getPeople = (count) =>
+//   new Promise((resolves, rejects) => {
+//     const api = `https://api.randomuser.me/?nat=US&results=${count}`;
+//     const request = new XMLHttpRequest();
+//     request.open("GET", api);
+//     request.onload = () =>
+//       request.status === 200
+//         ? resolves(JSON.parse(request.response).results)
+//         : reject(Error(request.statusText));
+//     request.onerror = (err) => rejects(err);
+//     request.send();
+//   });

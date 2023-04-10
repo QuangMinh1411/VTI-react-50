d55df4ce63775b28d79f6a3e9733b5b77895441b
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2023-04-10 21:54:09 +0700

    BTVN

--- React/state-demo/src/App.js
+++ React/state-demo/src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 // import EmailInput from "./components/EmailInput";
 import Counter from "./components/Counter";
 function App() {
+  // const [number, setNumber] = useState(1);
+  // const handleChange = (e) => {
+  //   setNumber(e.target.value);
+  // };
   return (
     <div className="App">
       {/* <EmailInput /> */}
+      {/* <input type="number" onChange={handleChange} value={number} /> */}
       <Counter />
     </div>
   );
--- React/state-demo/src/components/Counter.js
+++ React/state-demo/src/components/Counter.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
-  let counter = 0;
-  const increment = () => {};
+  //   let counter = 0;
+  const [number, setNumber] = useState(1);
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    setCounter(function (counter) {
+      return counter + number;
+    });
+  };
+  const handleChange = (e) => {
+    setNumber(Number(e.target.value));
+  };
 
-  const decrement = () => {};
+  const decrement = () => {
+    setCounter((counter) => counter - number);
+  };
+
+  const reset = () => {
+    setCounter(0);
+  };
   return (
     <>
       <div>{counter}</div>
+      <input type="number" value={number} onChange={handleChange} />
       <button onClick={increment}>Increase</button>
       <button onClick={decrement}>Decrease</button>
+      <button onClick={reset}>Reset</button>
     </>
   );
 };
